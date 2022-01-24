9a4e036f20cc584af56710a102221038eac16d86
JL-coder <60306232+JL-coder@users.noreply.github.com>
2022-01-23 21:54:37 -0500

    Delete In class exercises.py

--- Applications Of Computer Science With Data Structures and Algorithms/Class Files/In class exercises.py
+++
@@ -1,8 +0,0 @@
-def to_string(self):
-    s = "Teacher: " + self.get_teacher_name()
-    s += "\nNb Students: " + str(self.get_NbStudents())
-    s += "\n---"
-    for a in self.get_studentList():
-        s += a.toString()
-        s += "\n"
-    return s
